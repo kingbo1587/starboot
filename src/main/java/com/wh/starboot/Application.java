102cdf1f5c00ae8b3df947e50767130d4952e2e5
zanghuaqian <zanghuaqian@weshare.com.cn>
2016-09-13 20:13:52 +0800

    ST-000: init project

---
+++ src/main/java/com/wh/starboot/Application.java
@@ -0,0 +1,48 @@
+package com.wh.starboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+@PropertySources({ @PropertySource("classpath:config/config.properties") })
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("==============SpringApplicationBuilder========!!!!!!!!!!!!!!");
+		System.out.println("==============SpringApplicationBuilder========!!!!!!!!!!!!!!");
+		System.out.println("==============SpringApplicationBuilder========!!!!!!!!!!!!!!");
+		
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+	/**
+	 * 解决post中文乱码
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean characterEncodingFilter() {
+		final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(characterEncodingFilter);
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+
+}
---
+++ src/main/java/com/wh/starboot/web/HelloController.java
@@ -0,0 +1,29 @@
+package com.wh.starboot.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+	@Value("${hello.rivulet}")
+	private String msg;
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World222!" + msg;
+	}
+
+	@RequestMapping("hello")
+	String first() {
+		return "hello";
+	}
+
+}
